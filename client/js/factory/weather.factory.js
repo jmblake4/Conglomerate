44c6b219d0fa1840adcb07e4532d472d593b7f2e
Steve Sartino <stevesartino@yahoo.com>
2015-11-05 13:28:03 -0600

    Added promise/callback info for debugging weather factory. Login is adding a basic header into API call, which is breaking the response from openweather server.

--- client/js/factory/weather.factory.js
+++ client/js/factory/weather.factory.js
@@ -2,15 +2,27 @@ angular.module('app').factory('Weather', ['$http', '$cacheFactory', '$rootScope'
 
 	var WeatherFeed = {};
 	
-	var reqHeaders = { headers: {
-		// "x-api-key": "26c35d41ead43cdf58f2b5b32d00cd45",
+	var reqHeaders = {
+		 "x-api-key": "26c35d41ead43cdf58f2b5b32d00cd45",
 		"Content-Type": "application/json"
-	}};
+	};
 
-	var urlPath = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=26c35d41ead43cdf58f2b5b32d00cd45';
+	var urlPath = encodeURI('http://api.openweathermap.org/data/2.5/forecast/city?id=524901');
 
 	WeatherFeed.getPosts = function() {
-		return $http.get(urlPath, reqHeaders);
+		console.log('making get request to: ' + urlPath);
+		return $http({
+			url: urlPath,
+			method: 'GET',
+			headers: reqHeaders
+		}).then(function(success) {
+			console.log('success!');
+			console.log(success);
+			return success;
+		}, function (err) {
+			console.log('error!');
+			console.error(err);
+		});
 	};
 
 	return WeatherFeed;
